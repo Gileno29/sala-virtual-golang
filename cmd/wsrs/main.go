cc2ac59b6602f0562cd277c867cb6b727951f6d4
Gileno <gileno.cr.duarte@gmail.com>
2024-08-05 17:59:06 -0300

    adicionado configuração de API

---
+++ cmd/wsrs/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/Gileno29/sala-virtual-golang/internal/api"
+	"github.com/Gileno29/sala-virtual-golang/internal/store/pgstore/pgstore"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf(
+		"user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("WSRS_DATABASE_USER"), os.Getenv("WSRS_DATABASE_PASSWORD"), os.Getenv("WSRS_DATABASE_HOST"), os.Getenv("WSRS_DATABASE_PORT"), os.Getenv("WSRS_DATABASE_NAME"),
+	))
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close() // antes da funcao retornar vai executar
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+	handler := api.NewHandeler(pgstore.New(pool))
+
+	go func() {
+		if err := http.ListenAndServe(":8080", handler); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				panic(err)
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+}
